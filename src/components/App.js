70bab0b535324de27bd1796d4d6f274474a4e27e
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-05-28 15:23:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,45 @@ import React, { useState, useEffect } from "react";
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
+  const [preview, setPreview] = useState('');
+  const [quote, setQuote] = useState('');
+
+  const handleKeyPress = (keyValue) => {
+    setPreview(preview + keyValue);
+  };
+
+  useEffect(() => {
+    if (preview === 'forty two') {
+      fetch('https://api.quotable.io/random')
+        .then(response => response.json())
+        .then(data => {
+          setQuote(data.content);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    } else {
+      setQuote('');
+    }
+  }, [preview]);
 
   return (
     <div className="keyboard">
-      <div className="preview"></div>
+      <div className="preview">{preview}</div>
       <div>
         {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
+          <button
+            key={key}
+            id={key === " " ? `key-space` : `key-${key}`}
+            onClick={() => handleKeyPress(key)}
+          >
             {key === " " ? "Space" : key.toUpperCase()}
           </button>
         ))}
       </div>
+      {quote && <div className="quote">{quote}</div>}
     </div>
   );
 };
 
-export default App;
+export default App;
